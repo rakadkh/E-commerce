fda5feb786a9c53f2353fd29c23033f75d137d83
Rakad <rakad.wise.man@gmail.com>
2020-04-23 11:48:25 +0300

    C

---
+++ site.js
@@ -0,0 +1,11 @@
+/*const dropdown = document.getElementById("box")
+
+const enterColor = function () {
+    dropdown.style.backgroundColor = "#c0392b"
+    dropdown.innerHTML = "AHH GO AWAY"
+}
+
+const leaveColor = function () {
+    dropdown.style.backgroundColor = "#1abc9c"
+    dropdown.innerHTML = "Hover over me!"
+}*/
